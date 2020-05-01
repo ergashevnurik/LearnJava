08deadc116030ee9434f6417f43c116f0c790ce6
User <ergashev.nurik01@mail.ru>
2020-05-01 12:37:37 +0500

    First Commit

---
+++ src/FirstLesson.java
@@ -0,0 +1,18 @@
+public class FirstLesson {
+    int f = 50; // instance variable
+    static int p = 100; // static variable
+    public static void main(String args[]) {
+        int a = 10; // narrowing
+        float b = (float) a;
+        System.out.println(b);
+        int x = 10; //widening
+        float y = x;
+        System.out.println(y);
+        int c = 130; // overflow
+        byte d = (byte) c;
+        System.out.println(d);
+    }
+    public void method() {
+        int n = 10; // local variable
+    }
+}
